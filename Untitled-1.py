44c29b42d70377a2a9904b5880f58c3646187ae8
Parva2000 <par.pirooz@gmail.com>
2026-04-04 10:53:44 +0100

    Create Untitled-1.py

---
+++ Untitled-1.py
@@ -0,0 +1,12 @@
+favourite_fruit = "blueberry"
+print(len(favourite_fruit))
+
+sentence = "I love blueberries!"
+print(len(sentence))
+
+last_char = favourite_fruit[len(favourite_fruit)-1]
+print(last_char)
+
+length = len(favourite_fruit)
+last_chars = favourite_fruit[length -4 :]
+print(last_chars)
